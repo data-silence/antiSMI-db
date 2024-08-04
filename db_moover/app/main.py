8acfd74c0985f6c8a0fe84dd21a7f24964de03f2
data-silence <enjoy-ds@pm.me>
2024-08-04 17:32:49 +0300

    start embedder operation with local api correctly

--- db_moover/app/main.py
+++ db_moover/app/main.py
@@ -21,7 +21,7 @@ async def move_news_to_new_db():
 
 
 async def upsert_embs():
-    api_client = NewsAPIClient("http://127.0.0.1:8000")
+    api_client = NewsAPIClient("http://host.docker.internal:8000")
     logger.info('Идёт получение новостей без эмбеддингов')
     news = await api_client.fetch_news_without_embeddings()
     logger.info(f'Получено {len(news)} новостей')
@@ -29,28 +29,29 @@ async def upsert_embs():
     logger.info('Начинается процесс генерации эмбеддингов для новостей')
     embs = await api_client.make_embs(news_list=news_list)
     update_list = [{"url": item["url"], "embedding": emb} for item, emb in zip(news, embs)]
-    logger.info('Начинается процесс вставки эмбеддингов')
     update_batch_result = await api_client.update_news_batch(update_list)
     logger.info(f'{update_batch_result}')
 
 
 async def main():
     try:
-        # await move_news_to_new_db()
-        await upsert_embs()
-
-        # scheduler = AsyncIOScheduler()
-        # scheduler.configure(timezone='Europe/Moscow')
+        #await move_news_to_new_db()
+        #await upsert_embs()
+        scheduler = AsyncIOScheduler()
+        scheduler.configure(timezone='Europe/Moscow')
 
         # scheduler.add_job(move_news_to_new_db, 'cron', hour='9-21/4, 23', minute=53, id='moving',
         #                   max_instances=10, misfire_grace_time=600)
-        # scheduler.add_job(upsert_embs, 'cron', hour='22', id='embedder',
-        #                   max_instances=10, misfire_grace_time=600)
+        scheduler.add_job(upsert_embs, 'cron', hour='22', id='embedder',
+                          max_instances=10, misfire_grace_time=600)
 
-        # scheduler.start()
+        scheduler.start()
+
+        # Бесконечное ожидание
+        await asyncio.get_event_loop().create_future()
 
         # while True:
-        #     await asyncio.sleep(1)
+        #    await asyncio.sleep(1)
 
     except Exception as e:
         logger.exception(e)
@@ -58,3 +59,4 @@ async def main():
 
 if __name__ == '__main__':
     asyncio.run(main())
+
